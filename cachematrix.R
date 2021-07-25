c1631262ea70c7f16477015a739b129ee7c2bc83
dbod <david.bodin.bzh@gmail.com>
2021-07-25 20:37:07 +0200

    Creation of makeCacheMatrix and cacheSolve
    makeCacheMatrix creates a list of 4 functions to set and get an original matrix and its inverted matrix in a cache.
    
    cacheSolve find the inverse of a matrix using the cache created by makeCacheMatrix.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,30 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  x_1 <- NULL
+  set <- function(mt) {
+    x <<- mt
+    x_1 <<- NULL
+  }
+  get <- function() x
+  set_inverted_matrix <- function(inverted_matrix) x_1 <<- inverted_matrix
+  get_inverted_matrix <- function() x_1
+  list(set = set, get = get,
+       set_inverted_matrix = set_inverted_matrix,
+       get_inverted_matrix = get_inverted_matrix)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inverted_matrix <- x$get_inverted_matrix()
+  if(!is.null(inverted_matrix)) {
+    message("getting inverted matrix from cached data")
+    return(inverted_matrix)
+  }
+  inverted_matrix <- solve(x$get(), ...)
+  x$set_inverted_matrix(inverted_matrix)
+  inverted_matrix
 }
